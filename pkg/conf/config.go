9d3f91924b8d41f34edb8af735fe87fff9acb5f8
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-05-21 02:18:41 +0300

    Rename conf struct

--- pkg/conf/config.go
+++ pkg/conf/config.go
@@ -4,12 +4,12 @@ import (
 	"github.com/caarlos0/env/v6"
 )
 
-type Struct struct {
+type App struct {
 	PrometheusBind string `env:"PROMETHEUS_BIND" envDefault:":2112"`
 }
 
-func ParseEnv() (*Struct, error) {
-	cfg := Struct{}
+func ParseEnv() (*App, error) {
+	cfg := App{}
 	err := env.Parse(&cfg)
 	if err != nil {
 		return nil, err
